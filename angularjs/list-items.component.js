0378633c003f9c1949b669e92fd36957838aa618
James Anderson <jsondev1.0@outlook.com>
2018-10-16 14:51:02 -0400

    Updated to include pagination

--- angularjs/list-items.component.js
+++ angularjs/list-items.component.js
@@ -3,12 +3,33 @@
 
   app.component('listItems', {
     templateUrl: '',
-    controller: function listItemController($log, ListItemService) {
+    controller: function ListItemsController($scope, $log, ListItemService) {
       var vm = this;
-      ListItemService.getData('').then(getSuccess).catch(getError);
+      vm.currentPage = 1;
+      vm.itemsPerPage = 20;
+      ListItemService.getData('Action Icons').then(getSuccess).catch(getError);
+
+
+      function setPagingData(page) {
+        vm.currentPage = page;
+        var pagedData = vm.allItems.slice(
+          (page - 1) * vm.itemsPerPage,
+          page * vm.itemsPerPage
+        );
+
+        vm.items = pagedData;
+      }
+
 
       function getSuccess(data) {
-        return vm.items = data;
+        vm.allItems = data
+        vm.totalItems = vm.allItems.length;
+
+        $scope.$watch('vm.currentPage', function() {
+          setPagingData(vm.currentPage);
+        });
+
+        return [vm.allItems, vm.totalItems];
       }
 
       function getError(response) {
@@ -17,4 +38,5 @@
     },
     controllerAs: 'vm'
   });
+
 })(APP);
